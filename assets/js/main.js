f7bc26e03caa643700c79fe74c03ed999f67decb
residentevil1256 <aperera98@yahoo.com>
2023-06-07 02:38:17 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -133,7 +133,7 @@
 
 			// Change toggle styling once we've scrolled past the header.
 				$header.scrollex({
-					bottom: '4vh',
+					bottom: '10vh',
 					enter: function() {
 						$navPanelToggle.removeClass('alt');
 					},
